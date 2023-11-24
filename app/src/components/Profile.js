69ea0d08e311878dacc4f25a754984560a4d782e
feehpadula <feeh.padula@gmail.com>
2023-11-24 20:28:49 -0300

    Profile component

--- app/src/components/Profile.js
+++ app/src/components/Profile.js
@@ -1,9 +1,27 @@
+import { useState } from "react";
+import Dropdown from "./Dropdown";
 import "./Profile.scss";
 
-function Profile() {
+function Profile({ routes }) {
+  const [open, setOpen] = useState(false);
+
+  function profileMenuHandleClick() {
+    setOpen(true);
+  }
+
+  const profileMenuHandleBlur = (e) => {
+    if (!e.currentTarget.contains(e.relatedTarget)) {
+      setOpen(false);
+    }
+  };
+
   return (
     <div className="top-menu-profile">
-      <span></span>
+      <span
+        onClick={profileMenuHandleClick}
+        style={{ pointerEvents: open && "none" }}
+      ></span>
+      {open && <Dropdown parentCallback={profileMenuHandleBlur} routes={routes} />}
     </div>
   );
 }
