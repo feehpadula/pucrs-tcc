93be76be7c5b8763224b2c07747d92be73d1882c
feehpadula <feeh.padula@gmail.com>
2023-11-24 20:29:08 -0300

    Select component

---
+++ app/src/components/Select.js
@@ -0,0 +1,13 @@
+import "./Select.scss";
+
+const Button = (props) => {
+  return (
+    <div className="select-container">
+      <select name="" id="">
+        <option value="valor1">Brasil</option>
+      </select>
+    </div>
+  );
+};
+
+export default Button;
