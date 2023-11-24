5b2ec2c16dbe5184a61a26ffa3631e22e63b743a
feehpadula <feeh.padula@gmail.com>
2023-11-24 20:24:23 -0300

    Adjust settings and pool

--- server/config.js
+++ server/config.js
@@ -1,11 +1,9 @@
 const config = {
-  db: {
-    host: "127.0.0.1",
-    user: "pucrs",
-    password: "82A?8])DMNWL+QaExG-*",
-    database: "pucrs",
-    connectTimeout: 10000,
-  },
-  listPerPage: 10,
+  connectionLimit: 10,
+  host: "127.0.0.1",
+  user: "pucrs",
+  password: "82A?8])DMNWL+QaExG-*",
+  database: "pucrs",
+  connectTimeout: 10000,
 };
 module.exports = config;
--- server/index.js
+++ server/index.js
@@ -26,5 +26,5 @@ app.use((err, req, res, next) => {
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`Listening at http://localhost:${port}/`);
 });
