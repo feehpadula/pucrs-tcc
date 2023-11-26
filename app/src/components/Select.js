8a6c7a8b506460ba7400153fb9f6239703005d54
feehpadula <feeh.padula@gmail.com>
2023-11-25 23:14:02 -0300

    Improve select component

--- app/src/components/Select.js
+++ app/src/components/Select.js
@@ -1,13 +1,22 @@
 import "./Select.scss";
 
-const Button = (props) => {
+const Select = (props) => {
   return (
-    <div className="select-container">
-      <select name="" id="">
-        <option value="valor1">Brasil</option>
-      </select>
+    <div className={`select-container ${props.className}`}>
+      {props.label && <label htmlFor={props.name}>{props.label}</label>}
+      <div className="select-background ml-15">
+        {props.data && (
+          <select name={props.name} id={props.id}>
+            {props.data.map((item, index) => (
+              <option key={index} value={item.value}>
+                {item.label}
+              </option>
+            ))}
+          </select>
+        )}
+      </div>
     </div>
   );
 };
 
-export default Button;
+export default Select;
