6015321f2bba218c63f10f9a69091169863881fb
feehpadula <feeh.padula@gmail.com>
2023-12-02 18:23:55 -0300

    Add trending and latest items to home

--- server/services/items.js
+++ server/services/items.js
@@ -46,7 +46,7 @@ async function getItem(itemId) {
       dataOutliers,
       (SELECT COUNT(*) FROM data WHERE data.itemsId = items.id) AS contributions
     FROM items
-    WHERE items.id=${itemId}`
+    WHERE items.id = ${itemId}`
   );
   connection.release();
 
@@ -62,10 +62,58 @@ async function getLatestItems() {
       id, 
       topicId,
       name,
-      creationDate
+      creationDate,
+      (SELECT COUNT(*) FROM data WHERE data.itemsId = items.id) AS totalContributions,
+      (SELECT DATE_FORMAT(date, '%d/%m/%Y') FROM data WHERE data.itemsId = items.id ORDER BY date DESC LIMIT 1) AS lastUpdate
     FROM items
     ORDER BY creationDate DESC
     LIMIT 10`
+  );
+  connection.release();
+
+  const data = helper.emptyOrRows(rows);
+
+  return data;
+}
+
+async function getTrendingItems() {
+  const connection = await pool.getConnection();
+  const [rows, fields] = await connection.query(
+    `SELECT 
+      items.id,
+      items.topicId,
+      items.name, 
+      COUNT(*) AS monthContributions,
+      (SELECT COUNT(*) FROM data WHERE data.itemsId = items.id) AS totalContributions,
+      DATE_FORMAT(MAX(data.date), '%d/%m/%Y') AS lastUpdate
+    FROM items
+      INNER JOIN data ON items.id = data.itemsId
+    WHERE data.date >= NOW() - INTERVAL 30 DAY
+    GROUP BY items.name
+    ORDER BY monthContributions DESC
+    LIMIT 10`
+  );
+  connection.release();
+
+  const data = helper.emptyOrRows(rows);
+
+  return data;
+}
+
+async function getSearchItems(query, page) {
+  const connection = await pool.getConnection();
+  const [rows, fields] = await connection.query(
+    `SELECT 
+      id, 
+      topicId,
+      name,
+      creationDate,
+      (SELECT COUNT(*) FROM data WHERE data.itemsId = items.id) AS contributions,
+      (SELECT date FROM data WHERE data.itemsId = items.id ORDER BY date DESC LIMIT 1) AS lastUpdate
+    FROM items
+    WHERE name LIKE '%${query}%'
+    ORDER BY lastUpdate DESC
+    LIMIT ${page * 10}, 11`
   );
   connection.release();
 
@@ -78,4 +126,6 @@ module.exports = {
   addItem,
   getItem,
   getLatestItems,
+  getTrendingItems,
+  getSearchItems,
 };
