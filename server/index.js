1872fd628adead9bdbdb8611162afc61bee90576
feehpadula <feeh.padula@gmail.com>
2023-12-24 16:50:40 -0300

    User api

--- server/index.js
+++ server/index.js
@@ -2,10 +2,12 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const port = 4000;
+require("dotenv").config();
 const topics = require("./routes/topics");
 const items = require("./routes/items");
 const data = require("./routes/data");
 const reports = require("./routes/reports");
+const user = require("./routes/user");
 
 app.use(express.json());
 app.use(
@@ -23,6 +25,7 @@ app.use("/topics", topics);
 app.use("/items", items);
 app.use("/data", data);
 app.use("/reports", reports);
+app.use("/user", user);
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
