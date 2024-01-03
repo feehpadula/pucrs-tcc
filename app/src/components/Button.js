eda7121e152586a4d0fe972692337324fa65bada
feehpadula <feeh.padula@gmail.com>
2024-01-02 23:12:16 -0300

    Manage disabled button

--- app/src/components/Button.js
+++ app/src/components/Button.js
@@ -2,7 +2,7 @@ import "./Button.scss";
 
 const Button = (props) => {
   return (
-    <button type={props.type} onClick={props.onClick}>
+    <button type={props.type} onClick={props.onClick} disabled={props.disabled}>
       {props.title}
       {props.icon}
     </button>
